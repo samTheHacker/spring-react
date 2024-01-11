5acbcd7368f1a827c4b5e194353a05fbd6af8e57
Sammy D <mobilvodoo@googlemail.com>
2024-01-11 01:01:10 +0000

    Rename BadRequest to BadRequestException

--- src/main/java/com/samdabbous/springreact/web/errors/BadRequestException.java
+++ src/main/java/com/samdabbous/springreact/web/errors/BadRequestException.java
@@ -4,20 +4,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class BadRequest extends RuntimeException {
-    public BadRequest() {
+public class BadRequestException extends RuntimeException {
+    public BadRequestException() {
         super();
     }
 
-    public BadRequest(String message) {
+    public BadRequestException(String message) {
         super(message);
     }
 
-    public BadRequest(String message, Throwable cause) {
+    public BadRequestException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public BadRequest(Throwable cause) {
+    public BadRequestException(Throwable cause) {
         super(cause);
     }
 }
