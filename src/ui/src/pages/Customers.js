e33fb8dd71128753091f0ba19205593504c282f1
Sammy D <mobilvodoo@googlemail.com>
2024-01-15 17:33:14 +0000

    Update Customers.js with setCustomers. Add localhost to package.json.

--- src/ui/src/pages/Customers.js
+++ src/ui/src/pages/Customers.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 
-const customers = [{"customerId":1,"firstName":"Cally","lastName":"Reynolds","emailAddress":"penatibus.et@lectusa.com","phoneNumber":"(901) 166-8355","address":"556 Lakewood Park, Bismarck, ND 58505"},{"customerId":2,"firstName":"Sydney","lastName":"Bartlett","emailAddress":"nibh@ultricesposuere.edu","phoneNumber":"(982) 231-7357","address":"4829 Badeau Parkway, Chattanooga, TN 37405"},{"customerId":3,"firstName":"Hunter","lastName":"Newton","emailAddress":"quam.quis.diam@facilisisfacilisis.org","phoneNumber":"(831) 996-1240","address":"2 Rockefeller Avenue, Waco, TX 76796"}]
+// const customers = [{"customerId":1,"firstName":"Cally","lastName":"Reynolds","emailAddress":"penatibus.et@lectusa.com","phoneNumber":"(901) 166-8355","address":"556 Lakewood Park, Bismarck, ND 58505"},{"customerId":2,"firstName":"Sydney","lastName":"Bartlett","emailAddress":"nibh@ultricesposuere.edu","phoneNumber":"(982) 231-7357","address":"4829 Badeau Parkway, Chattanooga, TN 37405"},{"customerId":3,"firstName":"Hunter","lastName":"Newton","emailAddress":"quam.quis.diam@facilisisfacilisis.org","phoneNumber":"(831) 996-1240","address":"2 Rockefeller Avenue, Waco, TX 76796"}]
 
 const Customers = () => {
+
+    const [customers, setCustomers] = useState([])
+
+    useLayoutEffect( ()=> {
+        const getCustomers = async() => {
+            const res = await fetch('api/customers');
+            const customers = await res.json();
+            setCustomers(customers);
+        }
+
+        getCustomers().catch(e => {
+            console.log('error fetching customers: ' + e);
+        })
+    })
     return(
         <table>
             <thead>
